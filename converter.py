58d4450ead138d18422c41baeaefdcf3120361fa
xzc1993 <xzc1993@gmail.com>
2013-12-13 13:26:54 +0100

    poprawka

--- converter.py
+++ converter.py
@@ -7,6 +7,14 @@ class Font:
 class FontLoader:
     def loadFont(self, directory):
         pass
+
+
+class TextDrawer:
+    def setFont(self, font):
+        pass
+
+    def draw(self, text):
+        pass
         
 t = getText()
 f = FontLoader().loadFont('fancyFont/')
