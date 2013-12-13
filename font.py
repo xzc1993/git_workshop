40c9cc3116fde72b5ff61f22709503e8dc6e2729
xzc1993 <xzc1993@gmail.com>
2013-12-13 13:56:12 +0100

    Naprawiony plik font.py

--- font.py
+++ font.py
@@ -7,22 +7,21 @@ class FontLoader:
     def loadFont(self, directory):
         font = Font("fancy")
 
-        font.letters['G'] = r'''
-  ___ 
- / __)
-( (_ \
- \___/
-'''
-		font.letters['I'] = r'''
-  __  
- (  ) 
-  )(  
- (__) 
-'''
-        font.letters['T'] = r'''
- ____ 
-(_  _)
-  )(  
- (__) 
-'''
+        font.letters['G'] = r'
+'  ___ '
+' / __)'
+'( (_ \'
+' \___/'
+
+		font.letters['I'] = r''
+'  __ ' 
+' (  )'
+'  )( '
+' (__)' 
+
+        font.letters['T'] = r''
+' ____ '
+'(_  _)'
+'  )(  '
+' (__) '
         return font
